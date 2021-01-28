f6257e43bf3957b058c63436fdcfd20641822eaa
Ovenoboyo <ovenoboyo@gmail.com>
2021-01-28 14:04:58 +0530

    WIP: Audio stream using datachannels

--- src/services/syncHandler.ts
+++ src/services/syncHandler.ts
@@ -2,7 +2,7 @@ import { Socket, io, ManagerOptions } from 'socket.io-client'
 import { PeerMode } from '@/store/sync/syncState'
 
 export class SyncHolder {
-  private peerConnection: { [key: string]: RTCPeerConnection } = {}
+  private peerConnection: { [key: string]: { peer: RTCPeerConnection | null; channel: RTCDataChannel | null } } = {}
   private socketConnection: Socket
   private audioElement: ExtendedHtmlAudioElement | null = null
   private peerMode: PeerMode = PeerMode.UNDEFINED
@@ -10,6 +10,7 @@ export class SyncHolder {
   private stream: MediaStream | null = null
   private BroadcasterID: string = ''
   private isNegotiating: { [id: string]: boolean } = {}
+  private dataChannelOpen: boolean = false
   private connectionOptions: Partial<ManagerOptions> = {
     forceNew: true,
     reconnection: true,
@@ -40,10 +41,61 @@ export class SyncHolder {
     return peer
   }
 
+  private setPeer(id: string, peer: RTCPeerConnection) {
+    if (this.peerConnection[id]) this.peerConnection[id].peer = peer
+    else this.peerConnection[id] = { peer: peer, channel: null }
+  }
+
+  private setChannel(id: string, channel: RTCDataChannel) {
+    if (this.peerConnection[id]) this.peerConnection[id].channel = channel
+    else this.peerConnection[id] = { peer: null, channel: channel }
+  }
+
+  private makeDataChannel(id: string, peer: RTCPeerConnection) {
+    let channel = peer.createDataChannel('test-label')
+    channel.binaryType = 'arraybuffer'
+    channel.onopen = (event) => {
+      console.log('channel open')
+      this.dataChannelOpen = true
+    }
+    channel.onmessage = function (event) {
+      console.log(event.data)
+    }
+    this.setChannel(id, channel)
+  }
+
+  public gotFile(buffer: Buffer) {
+    const chunkSize = 16384
+    if (this.dataChannelOpen) {
+      for (let i in this.peerConnection) {
+        let byteLength = buffer.byteLength
+        let byteOffset = 0
+        let byteEnd = byteOffset + chunkSize
+        console.log('byteLength: ' + byteLength)
+        while (byteOffset < byteLength - 1) {
+          this.peerConnection[i].channel?.send(buffer.slice(byteOffset, byteEnd))
+
+          byteOffset = byteEnd
+          byteEnd = byteOffset + chunkSize
+
+          if (byteEnd >= byteLength) {
+            byteEnd = byteLength - 1
+          }
+          console.log('byteOffset: ' + byteOffset)
+          console.log('byteEnd: ' + byteEnd)
+        }
+      }
+    }
+  }
+
+  private handleDataChannel(channel: RTCDataChannel) {
+    channel.onopen = (event) => {}
+  }
+
   private addRemoteCandidate() {
     this.socketConnection.on('candidate', (id: string, candidate: RTCIceCandidate) => {
       console.log('got remote candidate and adding it')
-      this.peerConnection[id]?.addIceCandidate(new RTCIceCandidate(candidate))
+      this.peerConnection[id].peer?.addIceCandidate(new RTCIceCandidate(candidate))
       // }
     })
   }
@@ -77,7 +129,7 @@ export class SyncHolder {
 
   private onAnswer() {
     this.socketConnection.on('answer', (id: string, description: RTCSessionDescription) => {
-      if (this.isNegotiating) this.peerConnection[id].setRemoteDescription(description)
+      if (this.isNegotiating) this.peerConnection[id].peer!.setRemoteDescription(description)
     })
   }
 
@@ -111,6 +163,16 @@ export class SyncHolder {
     }
   }
 
+  private onDataChannel(id: string, peer: RTCPeerConnection) {
+    peer.ondatachannel = (event) => {
+      let channel = event.channel
+      channel.onmessage = function (event) {
+        // console.log(event.data)
+      }
+      this.setChannel(id, channel)
+    }
+  }
+
   public joinRoom(id: string) {
     this.socketConnection.emit('joinRoom', id)
   }
@@ -136,35 +198,40 @@ export class SyncHolder {
 
   private setupInitiator(id: string) {
     let peer = this.makePeer(id)
+    this.makeDataChannel(id, peer)
+
     this.needsNegotiation(id, peer)
 
-    if (this.audioElement!.src || this.audioElement!.srcObject) {
-      this.handleStream(peer)
-    } else {
-      this.audioElement!.onloadeddata = () => {
-        this.handleStream(peer)
-      }
-    }
+    // if (this.audioElement!.src || this.audioElement!.srcObject) {
+    //   this.handleStream(peer)
+    // } else {
+    //   this.audioElement!.onloadeddata = () => {
+    //     this.handleStream(peer)
+    //   }
+    // }
 
-    this.peerConnection[id] = peer
+    // this.peerConnection[id] = peer
+    this.setPeer(id, peer)
   }
 
   private setupWatcher(id: string, description: RTCSessionDescription) {
     // Setup watcher listeners
     let peer = this.makePeer(id)
-    this.onStream(id, peer)
+    this.onDataChannel(id, peer)
+    // this.onStream(id, peer)
 
     peer
       .setRemoteDescription(description)
       .then(() => peer.createAnswer())
       .then((sdp) => peer.setLocalDescription(sdp))
       .then(() => this.socketConnection.emit('answer', id, peer.localDescription))
-    this.peerConnection[id] = peer
+
+    // this.peerConnection[id] = peer
+    this.setPeer(id, peer)
   }
 
   private onBroadcasterChange() {
     this.socketConnection.on('broadcasterChange', (id: string) => {
-      console.log('got change')
       this.BroadcasterID = id
     })
   }
